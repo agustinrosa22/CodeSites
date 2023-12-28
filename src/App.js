feb09444082372f38fb870ec82f0437d615ef1e6
agustinrosa22 <agustinrosa1234@gmail.com>
2023-12-28 18:08:12 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/navBar';
+import Footer from './Components/Footer/footer';
 function App() {
   return (
     <div className="App">
     <NavBar/>
+    
+    <Footer/>
     </div>
   );
 }
