264d0f7e7c0768039aeadc9cf0d24b67c5ab40a9
agustinrosa22 <agustinrosa1234@gmail.com>
2023-12-28 18:49:16 -0300

    Presentation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/navBar';
 import Footer from './Components/Footer/footer';
+import Presentation from './Views/Presentation/presentation';
 function App() {
   return (
     <div className="App">
     <NavBar/>
-    
+    <div>
+    <Presentation/>
+    </div>
     <Footer/>
     </div>
   );
---
+++ src/Views/Presentation/presentation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './presentation.module.css'
+import defaultLogo from '../../Assets/internet.png'
+const Presentation = () => {
+    return (
+        <div className={styles.presentation}>
+       <img className={styles.image} src={defaultLogo} alt="" />
+       <p className={styles.text}>Hola, somos <a href="enlace-a-linkedin-de-agustin" target="_blank">Agustin Rosa</a>, <a href="enlace-a-linkedin-de-franco" target="_blank">Franco Bodas</a>, y <a href="enlace-a-linkedin-de-julio" target="_blank">Julio Gutierrez</a>. el equipo de <strong>CodeSites</strong>. Nos apasiona el mundo de la programación y estamos comprometidos en brindar soluciones creativas y eficientes. Conformado por programadores dedicados, nuestro equipo está listo para enfrentar cualquier desafío técnico.</p>
+
+ </div>
+    );
+};
+
+export default Presentation;
