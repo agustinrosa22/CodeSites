cb38999ae1da6cd74e0d190544a1249d3516e6bb
agustinrosa22 <agustinrosa1234@gmail.com>
2023-12-28 18:02:21 -0300

    logo

--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './navBar.module.css'
-
+import defaultLogo from '../../Assets/internet.png'
 const NavBar = () => {
     return (
         <div className={styles.navBar}>
+        <img className={styles.logo} src={defaultLogo} alt="" />
         <h2 className={styles.title}>CodeSites</h2>
         </div>
     );
