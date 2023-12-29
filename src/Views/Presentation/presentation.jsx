cc52b20a109e3fb6f9c200dc27582b83ed1c1ea1
agustinrosa22 <agustinrosa1234@gmail.com>
2023-12-28 23:06:38 -0300

    link

--- src/Views/Presentation/presentation.jsx
+++ src/Views/Presentation/presentation.jsx
@@ -5,7 +5,12 @@ const Presentation = () => {
     return (
         <div className={styles.presentation}>
        <img className={styles.image} src={defaultLogo} alt="" />
-       <p className={styles.text}>Hola, somos <a href="enlace-a-linkedin-de-agustin" target="_blank">Agustin Rosa</a>, <a href="enlace-a-linkedin-de-franco" target="_blank">Franco Bodas</a>, y <a href="enlace-a-linkedin-de-julio" target="_blank">Julio Gutierrez</a>. el equipo de <strong>CodeSites</strong>. Nos apasiona el mundo de la programación y estamos comprometidos en brindar soluciones creativas y eficientes. Conformado por programadores dedicados, nuestro equipo está listo para enfrentar cualquier desafío técnico.</p>
+       <p className={styles.text}>Hola, somos 
+       <a href="https://www.linkedin.com/in/nestor-agustin-rosa" target="_blank">Agustin Rosa</a>, 
+       <a href="https://www.linkedin.com/in/franco-bodas-003298212/" target="_blank"> Franco Bodas</a>,
+        y <a href="https://www.linkedin.com/in/julio-ezequiel-gutierrez/" target="_blank">Julio Gutierrez</a>. 
+        el equipo de <strong>CodeSites</strong>. Nos apasiona el mundo de la programación y estamos comprometidos en brindar soluciones creativas y eficientes. 
+        Conformado por programadores dedicados, nuestro equipo está listo para enfrentar cualquier desafío técnico.</p>
 
  </div>
     );
