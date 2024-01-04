3f5007e5b007f46ed224ee3c333ff32f24026b1c
agustinrosa22 <agustinrosa1234@gmail.com>
2024-01-04 01:09:49 -0300

    ortografia

--- src/Views/Presentation/presentation.jsx
+++ src/Views/Presentation/presentation.jsx
@@ -6,8 +6,7 @@ const Presentation = () => {
         <div className={styles.presentation}>
        <img className={styles.image} src={defaultLogo} alt="" />
        <p className={styles.text}>Hola, somos 
-       <a href="https://www.linkedin.com/in/nestor-agustin-rosa" target="_blank">Agustin Rosa</a>, 
-       <a href="https://www.linkedin.com/in/franco-bodas-003298212/" target="_blank"> Franco Bodas</a>,
+       <a href="https://www.linkedin.com/in/nestor-agustin-rosa" target="_blank">Agustin Rosa</a>, <a href="https://www.linkedin.com/in/franco-bodas-003298212/" target="_blank">Franco Bodas</a>
         y <a href="https://www.linkedin.com/in/julio-ezequiel-gutierrez/" target="_blank">Julio Gutierrez</a>. 
         el equipo de <strong>CodeSites</strong>. Nos apasiona el mundo de la programación y estamos comprometidos en brindar soluciones creativas y eficientes. 
         Conformado por programadores dedicados, nuestro equipo está listo para enfrentar cualquier desafío técnico.</p>
