01841e7ac3e63f03ecbce686d88dde7d138c30f5
agustinrosa22 <agustinrosa1234@gmail.com>
2024-01-10 20:04:50 -0300

    css-presentation

--- src/Views/Presentation/presentation.jsx
+++ src/Views/Presentation/presentation.jsx
@@ -6,16 +6,16 @@ const Presentation = () => {
     return (
         <div className={styles.presentation}>
             <img className={styles.image} src={defaultLogo} alt="" />
-            <div>
+          
 
-                <h2 className={styles.presentation}>Bienvenido a CodeSites</h2>
-            </div>
-            <div>
-                <h3 className={styles.presentation}>Transformamos Ideas en Experiencias Digitales Inolvidables</h3>
-            </div>
-            <p>En CodeSites fusionamos creatividad y tecnología para llevar tu visión al mundo digital de manera impactante. Descubre cómo podemos impulsar tu presencia en línea y potenciar tu negocio.</p>
+                <h2 className={styles.title}>Bienvenido a CodeSites</h2>
+           
+         
+                <h3 className={styles.secondTitle}>Transformamos Ideas en Experiencias Digitales Inolvidables</h3>
+         
+            <p className={styles.text}>En CodeSites fusionamos creatividad y tecnología para llevar tu visión al mundo digital de manera impactante. Descubre cómo podemos impulsar tu presencia en línea y potenciar tu negocio.
 
-            <p className={styles.text}>Hola, somos
+            Hola, somos
                 <a href="https://www.linkedin.com/in/nestor-agustin-rosa" target="_blank">Agustin Rosa</a>, <a href="https://www.linkedin.com/in/franco-bodas-003298212/" target="_blank">Franco Bodas</a>
                 y <a href="https://www.linkedin.com/in/julio-ezequiel-gutierrez/" target="_blank">Julio Gutierrez</a>.
                 el equipo de <strong>CodeSites</strong>. Nos apasiona el mundo de la programación y estamos comprometidos en brindar soluciones creativas y eficientes.
